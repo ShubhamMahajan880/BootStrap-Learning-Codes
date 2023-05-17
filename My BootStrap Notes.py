d2260fe89a03f3585391944757593aa569fa83b1
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2023-05-17 18:56:04 +0530

    Add files via upload

---
+++ My BootStrap Notes.py
@@ -0,0 +1,15 @@
+#1) Colors in Bootstrap having name as:-
+
+1) .text-primary (Blue)
+2) .text-danger (red)
+3) .text-success (Green)
+4) .text-warning (yellow)
+5) text-info (SkyBLue)
+6) .text-light (white)
+
+#2) Container class me margin dene k lye my - n ya mx - n use kre ... where n is pixels in y and x axis respectively ml and mr can also be used ( margin-left) and (margin-right)
+
+#3) Displayflex is uses for align the itmes ....in BOOTSTRAP
+    d-flex justify-content-center align-items-center 
+
+#4) class="img-fluid" tag is uses for Responsiveness of image
